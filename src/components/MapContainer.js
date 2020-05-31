02dc1729481084abe62c280158510d28c9e9635b
JibbityJobbity <jibbity@pm.me>
2020-05-31 14:17:18 +1200

    Add container for the map

---
+++ src/components/MapContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MapContainer() {
+	return (
+		<div className="col-sm-8">
+			<div id="map" style={{ height: "32em" }}></div>
+		</div>
+	)
+}
+
+export default MapContainer
