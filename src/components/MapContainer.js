d7673376818b7f1c247bde750c5d7c395676303f
JibbityJobbity <jibbity@pm.me>
2020-06-02 16:11:24 +1200

    Load places API

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 function MapContainer() {
 	var map;
+	var places;
 
 	// Create the script tag, set the appropriate attributes
 	var script = document.createElement('script');
-	script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyA3u6XxQ1moE4MMx6J_Mw2i2mfjllFwPGo&callback=initMap';
+	script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyA3u6XxQ1moE4MMx6J_Mw2i2mfjllFwPGo&callback=initMap&libraries=places';
 	script.defer = true;
 	script.async = true;
 
@@ -14,7 +15,8 @@ function MapContainer() {
 		map = new this.window.google.maps.Map(document.getElementById('map'), {
 			zoom: 5,
 			center: {lat: -41.0723336, lng: 171.5579181}
-		})
+		});
+		places = new this.window.google.maps.places.PlacesService(map);
 	};
 
 	// Append the 'script' element to 'head'
